bcb4652af3b8eff5e1028272c9c2c2c4cc7d9b0a
Andy Perez <andyperez54@gmail.com>
2018-04-07 11:29:39 -0700

    initial restructuring of bolt_group.py

--- mechpy/bolted_joints/bolt_group.py
+++ mechpy/bolted_joints/bolt_group.py
@@ -6,7 +6,7 @@ Version:
     Python 3.x
 
 Required modules:
-    - numpy
+    - np
     - math
     - csv
 
@@ -49,14 +49,12 @@ TODO:
 """
 
 import math
-import numpy
+import numpy as np
 import csv
 
 
 class Fastener(object):
     """Fastener class containing an ID, location in space, and load vector"""
-
-    count = 0
 
     def __init__(self, ID, xyz, diameter, E, G, l):
         self.ID = ID
@@ -65,7 +63,6 @@ class Fastener(object):
         self.E = E
         self.G = G
         self.l = l
-        Fastener.count += 1
         self.area = (diameter ** 2) * math.pi / 4
         self.force = [0, 0, 0]
 
@@ -80,14 +77,10 @@ class Fastener(object):
 class Load(object):
     """Load class containing location, force, and moment vectors"""
 
-    count = 0
-
-    def __init__(self, ID, xyz, force, moment):
-        self.ID = ID
+    def __init__(self, xyz=[0, 0, 0], force=[0, 0, 0], moment=[0, 0, 0]):
         self.xyz = xyz
         self.force = force
         self.moment = moment
-        Load.count += 1
 
     def __repr__(self):
         return "Load ID: %s\nLocation: %s\nForce: %s\nMoment: %s" \
@@ -97,194 +90,217 @@ class Load(object):
                   self.moment)
 
 
-# Request the mapping CSV file from the user. The format should be:
-# x,y,z,load_in_x,load_in_y,load_in_z,diameter
-fastPath = input("Fastener info *.csv file: ")
-loadsPath = input("Applied loads *.csv file: ")
+class LoadSet(list):
+    """A set of loads"""
 
-# Import applied Loads
-with open(loadsPath, "r") as inputLoads:
-    appliedload = csv.DictReader(inputLoads)
+    def __init__(self)
 
-    L = []
-    for ld in appliedload:
-        loadID = int(ld['ID'])
-        loc = [float(ld['x']), float(ld['y']), float(ld['z'])]
-        f = [float(ld['fx']), float(ld['fy']), float(ld['fz'])]
-        m = [float(ld['mx']), float(ld['my']), float(ld['mz'])]
+    def sumLoads(loadList, appLocation):
+        """Determine the resultant load applied at the CG"""
+        # sum the forces
+        forces = [load.force for load in loadList]
+        sumForce = [sum(f) for f in zip(*forces)]
 
-        L.append(Load(loadID, loc, f, m))
+        # sum of the applied moments
+        moments = [load.moment for load in loadList]
+        sumMoment = [sum(m) for m in zip(*moments)]
 
-# Import fastener mapping file
-with open(fastPath, "r") as inputFasteners:
-    fastenerLines = csv.DictReader(inputFasteners)
+        # translate and add moments induced by forces
+        # Mx = Sum(yFz - (zFy-CGy))
+        # My = Sum(zFx - (xFz-CGz))
+        # Mz = Sum(xFy - (yFx-CGx))
+        for load in loadList:
+            sumMoment[0] += load.force[1] * (appLocation[2] - load.xyz[2]) - \
+                            load.force[2] * (appLocation[1] - load.xyz[1])
 
-    F = []
-    # Create a list of Fastener objects from each line in the mapping file
-    for fstnr in fastenerLines:
-        # Parse each line for location, load, and diameter information
-        fastID = int(fstnr['ID'])
-        location = [float(fstnr['x']), float(fstnr['y']), float(fstnr['z'])]
-        diameter = float(fstnr['dia'])
-        E = float(fstnr['E'])
-        G = float(fstnr['G'])
-        l = float(fstnr['l'])
+            sumMoment[1] += load.force[2] * (appLocation[0] - load.xyz[0]) - \
+                            load.force[0] * (appLocation[2] - load.xyz[2])
 
-        # Replace each list item in F with Fastener objects
-        F.append(Fastener(fastID, location, diameter, E, G, l))
+            sumMoment[2] += load.force[0] * (appLocation[1] - load.xyz[1]) - \
+                            load.force[1] * (appLocation[0] - load.xyz[0])
 
+        sumLoadID = Load.count * 100 + 1
 
-def findCG(fastenerList):
-    """Finds the center of gravity of the fastener group"""
-    CG = [0, 0, 0]
-    sumArea = 0
+        return Load(sumLoadID, appLocation, sumForce, sumMoment)
 
-    for fstnr in fastenerList:
-        # Calculate the sum of the products
-        for i, component in enumerate(fstnr.xyz):
-            CG[i] += component * fstnr.area
+    def fromCSV(loadsPath):
+        """Import applied loads from a CSV"""
 
-        # Calculate the sum of the areas
-        sumArea += fstnr.area
+        with open(loadsPath, "r") as inputLoads:
+            appliedload = csv.DictReader(inputLoads)
 
-    # Divide sum of products by sum of areas
-    for i, product in enumerate(CG):
-        CG[i] = product / sumArea
+            L = []
+            for ld in appliedload:
+                loadID = int(ld['ID'])
+                loc = [float(ld['x']), float(ld['y']), float(ld['z'])]
+                f = [float(ld['fx']), float(ld['fy']), float(ld['fz'])]
+                m = [float(ld['mx']), float(ld['my']), float(ld['mz'])]
 
-    return CG
+                L.append(Load(loadID, loc, f, m))
 
 
-def sumLoads(loadList, appLocation):
-    """Determine the resultant load applied at the CG"""
-    # sum the forces
-    forces = [load.force for load in loadList]
-    sumForce = [sum(f) for f in zip(*forces)]
+class FastenerGroup(list):
+    """A fastener group"""
 
-    # sum of the applied moments
-    moments = [load.moment for load in loadList]
-    sumMoment = [sum(m) for m in zip(*moments)]
+    def __init__(self):
+        """Preserves list.__init__() and extends it"""
 
-    # translate and add moments induced by forces
-    # Mx = Sum(yFz - (zFy-CGy))
-    # My = Sum(zFx - (xFz-CGz))
-    # Mz = Sum(xFy - (yFx-CGx))
-    for load in loadList:
-        sumMoment[0] += load.force[1] * (appLocation[2] - load.xyz[2]) - \
-                        load.force[2] * (appLocation[1] - load.xyz[1])
+        super().__init__()
 
-        sumMoment[1] += load.force[2] * (appLocation[0] - load.xyz[0]) - \
-                        load.force[0] * (appLocation[2] - load.xyz[2])
+        if len(self) > 0:
+            self.__groupUpdate_()
 
-        sumMoment[2] += load.force[0] * (appLocation[1] - load.xyz[1]) - \
-                        load.force[1] * (appLocation[0] - load.xyz[0])
+    def append(self, newFastener):
+        """Preserves list.append() and extends it"""
 
-    sumLoadID = Load.count * 100 + 1
+        super().append(newFastener)
+        self.__groupUpdate_()
 
-    return Load(sumLoadID, appLocation, sumForce, sumMoment)
+    def fromCSV(self, fastPath):
+        """Import list of fasteners from CSV"""
 
+        with open(fastPath, "r") as inputFasteners:
+            fastenerLines = csv.DictReader(inputFasteners)
 
-# Begin Calculations
-groupCG = findCG(F)
-netLoad = sumLoads(L, groupCG)
-matA = numpy.zeros((Fastener.count * 3, Fastener.count * 3))  # coeff matrix
-matB = numpy.zeros(Fastener.count * 3)  # solution matrix
+            F = []
+            # Create a list of Fastener objects from each line in the mapping file
+            for fstnr in fastenerLines:
+                # Parse each line for location, load, and diameter information
+                fastID = int(fstnr['ID'])
+                location = [float(fstnr['x']), float(fstnr['y']), float(fstnr['z'])]
+                diameter = float(fstnr['dia'])
+                E = float(fstnr['E'])
+                G = float(fstnr['G'])
+                l = float(fstnr['l'])
 
-cSet = [[i, i+1, i+2] for i in range(0, 3 * Fastener.count, 3)]
-rSet = [[i+6, i+7, i+8] for i in range(0, 3 * (Fastener.count - 2), 3)]
-
-for i, j in enumerate(cSet):
-    # i = column fastener ID
-    # j = column fastener set
-    # Mx = yFz - zFy
-    # My = zFx - xFz
-    # Mz = xFy - yFx
-
-    Fx = j[0]
-    Fy = j[1]
-    Fz = j[2]
-
-    # fill in first three rows
-    matA[0][Fx] = 1  # sum of Fx
-    matA[1][Fy] = 1  # sum of Fy
-    matA[2][Fz] = 1  # sum of Fz
-
-    # fill in fourth row (sum of Mx at CG)
-    matA[3][Fy] = -(F[i].xyz[2] - groupCG[2])  # -zFy
-    matA[3][Fz] = +(F[i].xyz[1] - groupCG[1])  # +yFz
-
-    # fill in fifth row (sum of My at CG)
-    matA[4][Fx] = +(F[i].xyz[2] - groupCG[2])  # +zFx
-    matA[4][Fz] = -(F[i].xyz[0] - groupCG[0])  # -xFz
-
-    # fill in sixth row (sum of Mz at CG)
-    matA[5][Fx] = -(F[i].xyz[1] - groupCG[1])  # -yFx
-    matA[5][Fy] = +(F[i].xyz[0] - groupCG[0])  # +xFy
-
-    for u, w in enumerate(rSet):
-        # u = row fastener ID
-        # w = row fastener set
-
-        rX = w[0]
-        rY = w[1]
-        rZ = w[2]
-
-        matA[rX][Fy] = -(F[i].xyz[2] - F[u].xyz[2])  # -zFy
-        matA[rX][Fz] = +(F[i].xyz[1] - F[u].xyz[1])  # +yFz
-
-        matA[rY][Fx] = +(F[i].xyz[2] - F[u].xyz[2])  # +zFx
-        matA[rY][Fz] = -(F[i].xyz[0] - F[u].xyz[0])  # -xFz
-
-        matA[rZ][Fx] = -(F[i].xyz[1] - F[u].xyz[1])  # -yFx
-        matA[rZ][Fy] = +(F[i].xyz[0] - F[u].xyz[0])  # +xFy
+                # Replace each list item in F with Fastener objects
+                F.append(Fastener(fastID, location, diameter, E, G, l))
 
 
-# fill in the solution matrix (matB)
-for i in range(3):
-    matB[i] = -netLoad.force[i]
-    matB[i+3] = -netLoad.moment[i]
+    def findCG(self,fastenerList):
+        """Finds the center of gravity of the fastener group"""
+        CG = [0, 0, 0]
+        sumArea = 0
 
-# fill in the remaining rows
-for i, j in enumerate(rSet):
-    # i = fastener
-    # j = row
+        for fstnr in fastenerList:
+            # Calculate the sum of the products
+            for i, component in enumerate(fstnr.xyz):
+                CG[i] += component * fstnr.area
 
-    rX = j[0]
-    rY = j[1]
-    rZ = j[2]
+            # Calculate the sum of the areas
+            sumArea += fstnr.area
 
-    # Mx = (y_cg - y_i)F_znet - (z_cg - z_i)F_ynet + M_xnet
-    matB[rX] = - ((groupCG[1] - F[i].xyz[1]) * netLoad.force[2]
-                  - (groupCG[2] - F[i].xyz[2]) * netLoad.force[1]
-                  + netLoad.moment[0])
+        # Divide sum of products by sum of areas
+        for i, product in enumerate(CG):
+            CG[i] = product / sumArea
 
-    # My = (z_cg - z_i)F_xnet - (x_cg - x_i)F_znet + M_ynet
-    matB[rY] = -((groupCG[2] - F[i].xyz[2]) * netLoad.force[0]
-               - (groupCG[0] - F[i].xyz[0]) * netLoad.force[2]
-               + netLoad.moment[1])
+        return CG
 
-    # Mz = (x_cg - x_i)F_ynet - (y_cg - y_i)F_xnet + M_znet
-    matB[rZ] = -((groupCG[0] - F[i].xyz[0]) * netLoad.force[1]
-               - (groupCG[1] - F[i].xyz[1]) * netLoad.force[0]
-               + netLoad.moment[2])
+    def writetoCSV(self, fileName):
+        """Output resulting fastener loads to a CSV"""
 
-# Solve system of equations
-matSol = numpy.linalg.lstsq(matA, matB)[0]
+        with open(fileName, 'w') as writeFile:
+            writeFile.write("ID,Fx,Fy,Fz\n")
+            for fstnr in F:
+                writeFile.write(str(fstnr.ID))
+                for i in fstnr.force:
+                    writeFile.write(',' + str(i))
+                writeFile.write('\n')
 
-# Add resulting fastener loads to fastener objects
-for i, j in enumerate(cSet):
-    rX = j[0]
-    rY = j[1]
-    rZ = j[2]
+    def __groupUpdate_(self):
+        # Begin Calculations
+        groupCG = findCG(F)
+        netLoad = sumLoads(L, groupCG)
+        matA = np.zeros((Fastener.count * 3, Fastener.count * 3))  # coeff matrix
+        matB = np.zeros(Fastener.count * 3)  # solution matrix
 
-    F[i].force[0] = matSol[rX]
-    F[i].force[1] = matSol[rY]
-    F[i].force[2] = matSol[rZ]
+        cSet = [[i, i+1, i+2] for i in range(0, 3 * Fastener.count, 3)]
+        rSet = [[i+6, i+7, i+8] for i in range(0, 3 * (Fastener.count - 2), 3)]
 
-# Write resulting fastener loads to output file
-with open('fastener_loads.csv', 'w') as writeFile:
-    writeFile.write("ID,Fx,Fy,Fz\n")
-    for fstnr in F:
-        writeFile.write(str(fstnr.ID))
-        for i in fstnr.force:
-            writeFile.write(',' + str(i))
-        writeFile.write('\n')
+        for i, j in enumerate(cSet):
+            # i = column fastener ID
+            # j = column fastener set
+            # Mx = yFz - zFy
+            # My = zFx - xFz
+            # Mz = xFy - yFx
+
+            Fx = j[0]
+            Fy = j[1]
+            Fz = j[2]
+
+            # fill in first three rows
+            matA[0][Fx] = 1  # sum of Fx
+            matA[1][Fy] = 1  # sum of Fy
+            matA[2][Fz] = 1  # sum of Fz
+
+            # fill in fourth row (sum of Mx at CG)
+            matA[3][Fy] = -(F[i].xyz[2] - groupCG[2])  # -zFy
+            matA[3][Fz] = +(F[i].xyz[1] - groupCG[1])  # +yFz
+
+            # fill in fifth row (sum of My at CG)
+            matA[4][Fx] = +(F[i].xyz[2] - groupCG[2])  # +zFx
+            matA[4][Fz] = -(F[i].xyz[0] - groupCG[0])  # -xFz
+
+            # fill in sixth row (sum of Mz at CG)
+            matA[5][Fx] = -(F[i].xyz[1] - groupCG[1])  # -yFx
+            matA[5][Fy] = +(F[i].xyz[0] - groupCG[0])  # +xFy
+
+            for u, w in enumerate(rSet):
+                # u = row fastener ID
+                # w = row fastener set
+
+                rX = w[0]
+                rY = w[1]
+                rZ = w[2]
+
+                matA[rX][Fy] = -(F[i].xyz[2] - F[u].xyz[2])  # -zFy
+                matA[rX][Fz] = +(F[i].xyz[1] - F[u].xyz[1])  # +yFz
+
+                matA[rY][Fx] = +(F[i].xyz[2] - F[u].xyz[2])  # +zFx
+                matA[rY][Fz] = -(F[i].xyz[0] - F[u].xyz[0])  # -xFz
+
+                matA[rZ][Fx] = -(F[i].xyz[1] - F[u].xyz[1])  # -yFx
+                matA[rZ][Fy] = +(F[i].xyz[0] - F[u].xyz[0])  # +xFy
+
+
+        # fill in the solution matrix (matB)
+        for i in range(3):
+            matB[i] = -netLoad.force[i]
+            matB[i+3] = -netLoad.moment[i]
+
+        # fill in the remaining rows
+        for i, j in enumerate(rSet):
+            # i = fastener
+            # j = row
+
+            rX = j[0]
+            rY = j[1]
+            rZ = j[2]
+
+            # Mx = (y_cg - y_i)F_znet - (z_cg - z_i)F_ynet + M_xnet
+            matB[rX] = - ((groupCG[1] - F[i].xyz[1]) * netLoad.force[2]
+                          - (groupCG[2] - F[i].xyz[2]) * netLoad.force[1]
+                          + netLoad.moment[0])
+
+            # My = (z_cg - z_i)F_xnet - (x_cg - x_i)F_znet + M_ynet
+            matB[rY] = -((groupCG[2] - F[i].xyz[2]) * netLoad.force[0]
+                       - (groupCG[0] - F[i].xyz[0]) * netLoad.force[2]
+                       + netLoad.moment[1])
+
+            # Mz = (x_cg - x_i)F_ynet - (y_cg - y_i)F_xnet + M_znet
+            matB[rZ] = -((groupCG[0] - F[i].xyz[0]) * netLoad.force[1]
+                       - (groupCG[1] - F[i].xyz[1]) * netLoad.force[0]
+                       + netLoad.moment[2])
+
+        # Solve system of equations
+        matSol = np.linalg.lstsq(matA, matB)[0]
+
+        # Add resulting fastener loads to fastener objects
+        for i, j in enumerate(cSet):
+            rX = j[0]
+            rY = j[1]
+            rZ = j[2]
+
+            F[i].force[0] = matSol[rX]
+            F[i].force[1] = matSol[rY]
+            F[i].force[2] = matSol[rZ]
