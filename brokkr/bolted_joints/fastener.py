4732030694ce54952ce819bce22d285eed2edf94
Andy Perez <andyperez54@gmail.com>
2019-07-02 10:28:51 -0700

    Update fastener.py

--- brokkr/bolted_joints/fastener.py
+++ brokkr/bolted_joints/fastener.py
@@ -1,93 +1,81 @@
 """A fastener class for bolted joint calculations."""
 
-import math
-import numpy as np
+from brokkr.config import USYS
+from unyt import (
+    unyt_array
+)
+from unyt.dimensions import (
+    length,
+    pressure
+)
 
 
 class Fastener:
-    """Fastener class containing an ID, location in space, and load vector."""
+    """A fastener.
+
+    Attributes
+    ----------
+    xyz : 3x1 unyt.unyt_array
+        location of the fastener (``dim: length``)
+    dia : float
+        shakn diameter (``dim: length``)
+    E : float
+        Young's modulus of the fastener material (``dim: (mass)
+        /((length)*(time)**2)``)
+    G : float
+        shear modulus of the fastener material (``dim: (mass)
+        /((length)*(time)**2)``)
+    length : float
+        shank length (``dim: length``)
+    flex_method : {'Huth-bolt-metal', 'Huth-rivet-metal', 'Huth-bolt-graphite',
+                   'Boeing', 'Swift', 'Tate', 'Grumman'}
+        method for calculating fastener flexibility
+        (``default='Huth-bolt-metal'``)
+    usys : unyt.UnitSystem
+        fastener unit system (``default=USYS``)
+    """
+
+    _flex_types = ['Huth-bolt-metal', 'Huth-rivet-metal', 'Huth-bolt-graphite',
+                   'Boeing', 'Swift', 'Tate', 'Grumman']
+
+    _param_dims = {
+        'xyz': (length, ),
+        'dia': (length, ),
+        'E': (pressure, ),
+        'G': (pressure, ),
+        'length': (length, )
+    }
+
+    _param_limits = {
+        'dia': {'mn': 0, 'mx': None, 'condition': 'g'},
+        'E': {'mn': 0, 'mx': None, 'condition': 'g'},
+        'G': {'mn': 0, 'mx': None, 'condition': 'g'},
+        'length': {'mn': 0, 'mx': None, 'condition': 'g'}
+    }
+
+    __slots__ = ['xyz', 'dia', 'E', 'G', 'length', 'c', 'flex_method', 'usys']
 
     def __init__(self,
-                 ID,
                  xyz,
-                 diameter,
+                 dia,
                  E,
                  G,
                  length,
-                 wt=1,
-                 axis=[0, 0, 0]):
-        """Initialize the instance."""
+                 flex_method='Huth-bolt-metal',
+                 usys=USYS):
+        """Create a `Fastener`."""
 
-        self.ID = ID
         self.xyz = xyz
-        self.diameter = diameter
+        self.dia = dia
         self.E = E
         self.G = G
         self.length = length
-        self.force = [0, 0, 0]
-        self.wt = wt
-        self.axis = np.array(axis) / np.linalg.norm(axis)  # unit vector
 
-    def __repr__(self):
-        """Return the "official" Fastener string representation."""
+    def __setattr__(self, attr, val):
 
-        return "Fastener ID: %s\nLocation: %s\nForce: %s\nDiameter: %s" \
-               % (self.ID,
-                  self.xyz,
-                  self.force,
-                  self.diameter)
-
-    @property
-    def area(self):
-        """Cross-sectional area of the fastener."""
-
-        return (self.diameter ** 2) * math.pi / 4
-
-    @area.setter
-    def area(self, a):
-        """Determine the diameter if the area is set manually."""
-
-        self.diameter = math.sqrt(4 * a / math.pi)
-
-    @property
-    def stiffness(self):
-        """Return stiffness of the fastener in each direction.
-
-        The stiffness is calculated based on the diameter and material
-        moduli of the fastener. The x-direction is the shaft axis.
-
-        Note: this does not account for total behavior of the joints.
-        """
-
-        return {'x': self.E * self.area / self.length,
-                'y': self.G * self.area / self.length,
-                'z': self.G * self.area / self.length}
-
-    @property
-    def compliance(self):
-        """Return the compliance of the fastener in each direction.
-
-        The compliance in each direction is the inverse of the stiffness.
-        """
-
-        return {'x': self.length / (self.E * self.area),
-                'y': self.length / (self.G * self.area),
-                'z': self.length / (self.G * self.area)}
 
     @property
     def x(self):
-        "x-coordinate of Fastener."
+        """The x-coordinate of the fastener."""
 
-        return self.xyz[0]
-
-    @property
-    def y(self):
-        "y-coordinate of Fastener."
-
-        return self.xyz[1]
-
-    @property
-    def z(self):
-        "z-coordinate of Fastener."
-
-        return self.xyz[2]
+        return
