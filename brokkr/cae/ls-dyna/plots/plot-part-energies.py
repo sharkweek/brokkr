ec901fc52ee2ab0bd4cc87ef5eb484bcb1096f0c
Andy Perez <sharkweek@users.noreply.github.com>
2020-12-17 09:42:08 -0800

    Update plot-part-energies.py

--- brokkr/cae/ls-dyna/plots/plot-part-energies.py
+++ brokkr/cae/ls-dyna/plots/plot-part-energies.py
@@ -13,14 +13,15 @@ parts = [[0, 1, 'Part 1'],
          [2, 3, 'Part 3']]
 
 # create and populate dataframe with part data
-parts_df =pd.DataFrame(columns=['matsum_id', 'pid', 'title', 'data', 'trace'])
+parts_df = pd.DataFrame(columns=['matsum_id', 'pid', 'part_title', 'type',
+                                 'trace'])
 for part in parts:
     for field in energy_fields_m:
         df = pd.DataFrame(
             {'matsum_id': [part[0]],
              'pid': [part[1]],
-             'title': [part[2]],
-             'data': [field],
+             'part_title': [part[2] + " (" + str(part[1]) + ")"],
+             'type': [field[:-7]],
              'trace': [go.Scatter(x=binout.read('matsum', 'time'),
                                   y=binout.read('matsum', field).T[part[0]],
                                   name=field[:-7])]}
@@ -33,11 +34,11 @@ for field in energy_fields_g:
         pd.DataFrame({
             'matsum_id': [None],
             'pid': [None],
-            'title': ["Global"],
-            'data': [field],
+            'part_title': ["Global"],
+            'type': [field[:-7]],
             'trace': [go.Scatter(x=binout.read('glstat', 'time'),
                                  y=binout.read('glstat', field),
-                                 name=field)]
+                                 name="global_" + field[:-7])]
         })
     )
 
@@ -66,21 +67,54 @@ for trace in parts_df.trace:
     fig.add_trace(trace)
 
 # add dropdown menu to figure
-titles = [x for x in parts_df.title.unique()]
-buttons = [{'label': x,
-            'method': 'update',
-            'args': [{'visible': parts_df.title == x}]}
-            for x in parts_df.title.unique()]
-fig.update_layout(updatemenus=[
-    {
-        'active': 0,
-        'buttons': buttons,
-        'direction': "down",
-        'x': 0,
-        'y': 1.3,
-        'showactive': True,
-    }
-])
+dropdown_bypart = [{'label': x,
+                    'method': 'update',
+                    'args': [{'visible': parts_df.part_title == x,
+                             'name': parts_df.type}]}
+                   for x in parts_df.part_title.unique()]
+
+dropdown_bydir = [{'label': x,
+                   'method': 'update',
+                   'args': [{'visible': parts_df.type == x,
+                             'name': parts_df.part_title}]}
+                  for x in parts_df.type.unique()]
+
+fig.update_layout(updatemenus=[{'active': 0,
+                                'type': 'dropdown',
+                                'buttons': dropdown_bypart,
+                                'direction': "down",
+                                'x': 0.05,
+                                'xanchor': 'left',
+                                'y': 1.3,
+                                'showactive': True},
+                               {'active': 0,
+                                'type': 'dropdown',
+                                'buttons': dropdown_bydir,
+                                'direction': 'down',
+                                'x': 0.05,
+                                'xanchor': 'left',
+                                'y': 1.17,
+                                'showactive': True}])
+
+fig.add_annotation({'text': 'By Part: ',
+                    'xanchor': 'right',
+                    'x': 0.05,
+                    'xref': 'paper',
+                    'y': 1.28,
+                    'yref': 'paper',
+                    'showarrow': False})
+fig.add_annotation({'text': 'By Type: ',
+                    'xanchor': 'right',
+                    'x': 0.05,
+                    'xref': 'paper',
+                    'y': 1.15,
+                    'yref': 'paper',
+                    'showarrow': False})
 
 # print figure
-fig.write_html('part-energies.html')
+fig.write_html(
+    binout.read('glstat', 'title').strip().replace(' ', '-').lower()
+    + '-part-energies.html',
+    include_mathjax='cdn',
+    default_width=800,
+)
