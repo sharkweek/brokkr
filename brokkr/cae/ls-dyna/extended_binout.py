e9a5b08d082ecb610008d6f07d9b458a9045203e
Andy Perez <sharkweek@users.noreply.github.com>
2020-12-23 08:04:38 -0800

    Update extended_binout.py
    reformated to add `df` and `plot` subclasses to reference

--- brokkr/cae/ls-dyna/extended_binout.py
+++ brokkr/cae/ls-dyna/extended_binout.py
@@ -1,152 +1,212 @@
 from pandas import DataFrame
 from lasso.dyna import Binout
 
+
 class Extended_Binout(Binout):
-    def read(self, *args):
-        """Extends the `read` functionality of `Binout`"""
-        if args[0:2] == ('matsum', 'legend'):
-            return self._matsum_legend_df()
+    @property
+    def glstat(self, key=None):
+        """`glstat` fields."""
+
+        if 'glstat' in self.read():
+            return sorted(self.read('glstat'))
         else:
-            return super().read(*args)
+            raise ValueError("No 'glstat' data in `binout`")
 
-    def _matsum_legend_df(self):
-        """reformat the `matsum` legend as a pandas `DataFrame`"""
-        legend = super().read('matsum', 'legend')
-        titles = [legend[i:i + 80].strip() for i in range(0, len(legend), 80)]
-        parts = [list(x) for x
-                 in zip(range(0, len(titles)),
-                        super().read('matsum', 'ids'),
-                        titles)]
-        df = pd.DataFrame({
-            'pid': super().read('matsum', 'ids'),
-            'title': [legend[i:i + 80].strip()
-                      for i in range(0, len(legend), 80)]
-        })
+    @property
+    def matsum(self, key=None):
+        """`matsum` fields."""
 
-        return df
+        if 'matsum' in self.read():
+            return sorted(self.read('matsum'))
+        else:
+            raise ValueError("No 'matsum' data in `binout`")
 
-    def matsum_energy_plot(self, filename=None):
-        """Plot `matsum` energy fields as Plotly figure."""
+    @property
+    def nodfor(self, key=None):
+        """`nodfor` fields."""
 
-        energy_fields_m = [x for x
-                           in super().read('matsum') if x[-6:] == 'energy']
-        energy_fields_g = [x for x
-                           in super().read('glstat') if x[-6:] == 'energy']
+        if 'nodfor' in self.read():
+            return sorted(self.read('nodfor'))
+        else:
+            raise ValueError("No 'nodfor' data in `binout`")
 
-        # create and populate dataframe with part data
-        legend = self._matsum_legend_df()
-        plot_df = DataFrame(columns=['matsum_id',
-                                     'pid',
-                                     'part_title',
-                                     'type',
-                                     'trace'])
-        for part in legend.title:
-            for field in energy_fields_m:
-                rec = legend[legend.title == part]
+    @property
+    def rbdout(self, key=None):
+        """`rbdout` fields."""
+
+        if 'rbdout' in self.read():
+            return sorted(self.read('rbdout'))
+        else:
+            raise ValueError("No 'rbdout' data in `binout`")
+
+    @property
+    def ssstat(self, key=None):
+        """`ssstat` fields."""
+
+        if 'ssstat' in self.read():
+            return sorted(self.read('ssstat'))
+        else:
+            raise ValueError("No 'ssstat' data in `binout`")
+
+    @property
+    def df(self):
+        return self._DF(self)
+
+    class _DF:
+        """Inner class containing methods to return data as `DataFrames`."""
+
+        def __init__(self, outer):
+            """Reference the outer object for inner methods."""
+
+            self.outer = outer
+
+        @property
+        def matsum_legend(self):
+            """`matsum` legend as a pandas `DataFrame`"""
+
+            legend = self.outer.read('matsum', 'legend')
+            return DataFrame({
+                'pid': self.outer.read('matsum', 'ids'),
+                'title': [legend[i:i + 80].strip()
+                          for i in range(0, len(legend), 80)]
+            })
+
+    @property
+    def plot(self):
+        return self._Plot(self)
+
+    class _Plot:
+        """Inner class containing methods to return interactive plots."""
+
+        def __init__(self, outer):
+            """Reference the outer object for inner methods."""
+
+            self.outer = outer
+
+        def matsum_energy(self, filename=None):
+            """Plot `matsum` energy fields as Plotly figure."""
+
+            energy_fields_m = [
+                x for x in self.outer.read('matsum') if x[-6:] == 'energy'
+            ]
+            energy_fields_g = [
+                x for x in self.outer.read('glstat') if x[-6:] == 'energy'
+            ]
+
+            # create and populate dataframe with part data
+            legend = self.outer.df.matsum_legend
+            plot_df = DataFrame(columns=['matsum_id',
+                                         'pid',
+                                         'part_title',
+                                         'type',
+                                         'trace'])
+            for part in legend.title:
+                for field in energy_fields_m:
+                    rec = legend[legend.title == part]
+                    plot_df = plot_df.append(
+                        {"matsum_id": rec.index[0],
+                         "pid": rec.pid.iloc[0],
+                         "part_title": rec.title.iloc[0]
+                                       + " (" + str(rec.pid.iloc[0]) + ")",
+                         "type": field[:-7],
+                         "trace": Scatter(
+                            x=self.outer.read('matsum', 'time'),
+                            y=self.outer.read('matsum', field).T[rec.index[0]],
+                            name=field[:-7]
+                        )},
+                        ignore_index=True
+                    )
+
+            # add global data to dataframe
+            for field in energy_fields_g:
                 plot_df = plot_df.append(
-                    {"matsum_id": rec.index[0],
-                     "pid": rec.pid.iloc[0],
-                     "part_title": rec.title.iloc[0]
-                                   + " (" + str(rec.pid.iloc[0]) + ")",
-                     "type": field[:-7],
-                     "trace": Scatter(
-                         x=super().read('matsum', 'time'),
-                         y=super().read('matsum', field).T[rec.index[0]],
-                         name=field[:-7]
-                     )},
+                    {'matsum_id': None,
+                     'pid': None,
+                     'part_title': "Global",
+                     'type': field[:-7],
+                     'trace': Scatter(x=self.outer.read('glstat', 'time'),
+                                      y=self.outer.read('glstat', field),
+                                      name=field[:-7])},
                     ignore_index=True
                 )
 
-        # add global data to dataframe
-        for field in energy_fields_g:
-            plot_df = plot_df.append(
-                {'matsum_id': None,
-                 'pid': None,
-                 'part_title': "Global",
-                 'type': field[:-7],
-                 'trace': Scatter(x=super().read('glstat', 'time'),
-                                  y=super().read('glstat', field),
-                                  name=field[:-7])},
-                ignore_index=True
-            )
+            # create figure with traces
+            layout = Layout(title={'text': 'Part Energies',
+                                   'x': 0.5,
+                                   'xanchor': 'center'},
+                            xaxis={'title': 't (s)',
+                                   'tickformat': '.0s',
+                                   'hoverformat': '.3s',
+                                   'showspikes': True,
+                                   'spikethickness': 1},
+                            yaxis={'title': 'Energy (lbf-in)',
+                                   'tickformat': '.0s',
+                                   'hoverformat': '.3s',
+                                   'showspikes': True,
+                                   'spikethickness': 1},
+                            legend={'title': 'Energy Type'},
+                            hovermode='closest',
+                            height=500,
+                            width=800)
 
-        # create figure with traces
-        layout = Layout(title={'text': 'Part Energies',
-                               'x': 0.5,
-                               'xanchor': 'center'},
-                        xaxis={'title': 't (s)',
-                               'tickformat': '.0s',
-                               'hoverformat': '.3s',
-                               'showspikes': True,
-                               'spikethickness': 1},
-                        yaxis={'title': 'Energy (lbf-in)',
-                               'tickformat': '.0s',
-                               'hoverformat': '.3s',
-                               'showspikes': True,
-                               'spikethickness': 1},
-                        legend={'title': 'Energy Type'},
-                        hovermode='closest',
-                        height=500,
-                        width=800)
+            fig = Figure(layout=layout)
 
-        fig = Figure(layout=layout)
+            for trace in plot_df.trace:
+                fig.add_trace(trace)
 
-        for trace in plot_df.trace:
-            fig.add_trace(trace)
+            # add dropdown menu to figure
+            dropdown_bypart = [{'label': x,
+                                'method': 'update',
+                                'args': [{'visible': plot_df.part_title == x,
+                                        'name': plot_df.type}]}
+                                        for x in plot_df.part_title.unique()]
 
-        # add dropdown menu to figure
-        dropdown_bypart = [{'label': x,
-                            'method': 'update',
-                            'args': [{'visible': plot_df.part_title == x,
-                                      'name': plot_df.type}]}
-                                     for x in plot_df.part_title.unique()]
+            dropdown_bydir = [{'label': x,
+                               'method': 'update',
+                               'args': [{'visible': plot_df.type == x,
+                                         'name': plot_df.part_title}]}
+                              for x in plot_df.type.unique()]
 
-        dropdown_bydir = [{'label': x,
-                           'method': 'update',
-                           'args': [{'visible': plot_df.type == x,
-                                     'name': plot_df.part_title}]}
-                          for x in plot_df.type.unique()]
+            fig.update_layout(updatemenus=[{'active': 0,
+                                            'type': 'dropdown',
+                                            'buttons': dropdown_bypart,
+                                            'direction': "down",
+                                            'x': 0.05,
+                                            'xanchor': 'left',
+                                            'y': 1.3,
+                                            'showactive': True},
+                                           {'active': 0,
+                                            'type': 'dropdown',
+                                            'buttons': dropdown_bydir,
+                                            'direction': 'down',
+                                            'x': 0.05,
+                                            'xanchor': 'left',
+                                            'y': 1.17,
+                                            'showactive': True}])
 
-        fig.update_layout(updatemenus=[{'active': 0,
-                                        'type': 'dropdown',
-                                        'buttons': dropdown_bypart,
-                                        'direction': "down",
-                                        'x': 0.05,
-                                        'xanchor': 'left',
-                                        'y': 1.3,
-                                        'showactive': True},
-                                       {'active': 0,
-                                        'type': 'dropdown',
-                                        'buttons': dropdown_bydir,
-                                        'direction': 'down',
-                                        'x': 0.05,
-                                        'xanchor': 'left',
-                                        'y': 1.17,
-                                        'showactive': True}])
+            fig.add_annotation({'text': 'By Part: ',
+                                'xanchor': 'right',
+                                'x': 0.05,
+                                'xref': 'paper',
+                                'y': 1.28,
+                                'yref': 'paper',
+                                'showarrow': False})
+            fig.add_annotation({'text': 'By Type: ',
+                                'xanchor': 'right',
+                                'x': 0.05,
+                                'xref': 'paper',
+                                'y': 1.15,
+                                'yref': 'paper',
+                                'showarrow': False})
 
-        fig.add_annotation({'text': 'By Part: ',
-                            'xanchor': 'right',
-                            'x': 0.05,
-                            'xref': 'paper',
-                            'y': 1.28,
-                            'yref': 'paper',
-                            'showarrow': False})
-        fig.add_annotation({'text': 'By Type: ',
-                            'xanchor': 'right',
-                            'x': 0.05,
-                            'xref': 'paper',
-                            'y': 1.15,
-                            'yref': 'paper',
-                            'showarrow': False})
-
-        if filename is not None:
-            # print figure
-            fig.write_html(
-                super().read('glstat',
-                             'title'). strip().replace(' ', '-').lower()
-                + '-part-energies.html',
-                include_mathjax='cdn',
-                default_width=800,
-            )
-        else:
-            return fig
+            if filename is not None:
+                # print figure
+                fig.write_html(
+                    self.outer.read('glstat',
+                                    'title'). strip().replace(' ', '-').lower()
+                        + '-part-energies.html',
+                    include_mathjax='cdn',
+                    default_width=800,
+                )
+            else:
+                return fig
